8789af0367e96cd823daa0863d49565498ffec19
Hector Salazar <hecsalazarf@outlook.com>
2019-06-06 17:02:36 -0500

    Request csrf token if not existing

--- src/router/guards/index.js
+++ src/router/guards/index.js
@@ -1,14 +1,21 @@
 import { Cookies } from 'quasar'
+import axios from 'axios'
 
 const GlobalGuards = {
   beforeEach (to, from, next) {
-    const accessToken = Cookies.get('access_token')
+    const accessToken = Cookies.get('access-token')
     if (!accessToken && to.name === 'login') {
       next()
     } else if (!accessToken) {
       next({ name: 'login' })
     } else if (accessToken && to.name === 'login') {
       next({ path: from.path })
+    } else if (!Cookies.get('csrf-token')) {
+      // check for csrf token and request if not existing
+      axios.get('auth/ping').then(() => next()).catch(err => {
+        console.error(err)
+        next()
+      })
     } else {
       next() // always call next()!
     }
