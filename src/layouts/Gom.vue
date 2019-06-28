4fb0c2d997bb67fb27a4ce46a3fcc08da7b1722c
Hector Salazar <hecsalazarf@outlook.com>
2019-06-27 22:34:20 -0500

    Hide footer bar when not in main page

--- src/layouts/Gom.vue
+++ src/layouts/Gom.vue
@@ -2,12 +2,10 @@
   <q-layout view="lhh lpr lFf">
     <q-header reveal :reveal-offset="100">
       <transition mode="out-in" leave-active-class="animated fadeOut">
-        <component
-          :is="activeToolbar"
-        />
+        <component :is="activeToolbar"/>
       </transition>
     </q-header>
-    <q-footer>
+    <q-footer v-show="$route.name === 'orders' || $route.name === 'customers'">
       <q-tabs dense no-caps v-model="tab" active-color="white">
         <q-route-tab name="orders" :to="{ name: 'orders' }" label="Pedidos" icon="shopping_cart"/>
         <q-route-tab name="customers" :to="{ name: 'customers' }" label="Clientes" icon="contacts"/>
@@ -28,7 +26,8 @@ export default {
   name: 'GomLayout',
   components: {
     'h-main-toolbar': () => import('components/MainToolbar.vue'),
-    'h-selection-toolbar': () => import('components/order/SelectionToolbar.vue'),
+    'h-selection-toolbar': () =>
+      import('components/order/SelectionToolbar.vue'),
     'h-order-toolbar': () => import('components/order/DetailsToolbar.vue'),
     'h-customer-toolbar': () => import('components/customer/DetailsToolbar.vue')
   },
@@ -39,14 +38,10 @@ export default {
   },
   methods: {
     openURL,
-    ...mapActions([
-      'changeActiveToolbar'
-    ])
+    ...mapActions(['changeActiveToolbar'])
   },
   computed: {
-    ...mapGetters([
-      'activeToolbar'
-    ])
+    ...mapGetters(['activeToolbar'])
   },
   created () {
     this.changeActiveToolbar('h-main-toolbar')
