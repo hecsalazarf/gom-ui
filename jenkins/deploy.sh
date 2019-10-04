e80d7a0768a07007091f118dadf6d9737de22e2b
Hector Salazar <hecsalazarf@outlook.com>
2019-10-04 08:05:08 -0500

    chore:  replace ssh-steps-plugin with ssh-agent

---
+++ jenkins/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Tiny script to copy assets to the Gom UX root directory in the web server
+
+set -eo pipefail
+# -e  Exit immediately if a command exits with a non-zero status.
+# -o option-name
+# pipefail: the return value of a pipeline is the status of the last
+# command to exit with a non-zero status, or zero if no command exited with a non-zero status
+
+echo '--> Sending assets'
+# Send files and delete extraneous files from dest dirs
+rsync --delete -avzhe "ssh ${SSH_OPTIONS}" dist/ $SSH_USR@$REMOTE_HOST:$REMOTE_TEMP_DIR
+echo "--> Copying assets to web server directory"
+ssh $SSH_OPTIONS -l $SSH_USR $REMOTE_HOST "echo $SSH_PSW | sudo -S rm -rf $REMOTE_WEB_DIR*"
+ssh $SSH_OPTIONS -l $SSH_USR $REMOTE_HOST "echo $SSH_PSW | sudo -S cp -r $REMOTE_TEMP_DIR. $REMOTE_WEB_DIR"
