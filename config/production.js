97bf2412b86f21efff472d08c4395c3ba9e6f04f
hecsalazarf <36686213+hecsalazarf@users.noreply.github.com>
2022-05-19 09:51:23 -0500

    chore: Delete dummy VAPID public key
    Although the public key is invalid and for demo purposes, we delete it avoid confusion

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  VAPID_PUBLIC_KEY: 'BCmYhh00E-9sqJ3YG2F6FEAaHJ2DcXHd7wbIV-XfMP49oQnIKu282eY9m9oy51I1-Ofgfm1dc4iGyWNF2Lvc3j0',
+  VAPID_PUBLIC_KEY: 'PUBLIC_KEY_HERE',
   WS_ENDPOINT: 'wss://hlfapp.sytes.net/api/graphql',
   GA_MEASUREMENT_ID: 'UA-149998074-2'
 }
